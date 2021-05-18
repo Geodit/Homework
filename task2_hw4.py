91c1df803f9f50abe156b3fbb491f6bc21aeaeb5
geodit <geodit150@gmail.com>
2021-05-18 17:53:34 +0300

    lesson4, 2 задание закоммитил после просмотра разбора домашнего задания, ранее решил через перебор значений и с костылем

--- task2_hw4.py
+++ task2_hw4.py
@@ -4,3 +4,5 @@
 # Результат: [12, 44, 4, 10, 78, 123].
 list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 print([i for i in list if i > list[list.index(i)-1] and list.index(i) != 0 ])
+print('*'*30)
+print([list[i] for i in range(1,len(list)) if list[i] > list[i-1]])
